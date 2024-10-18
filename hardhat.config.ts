c56d5e34feb4a473d6ad74e2ad652393b1d696bb
muimui69 <moises_c2015@outlook.es>
2024-10-18 00:17:48 -0400

    add: zk SNARKS,pruebas de conocimiento cero, desplegado en alchemy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,8 +3,25 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-chai-matchers";
 
+import "@matterlabs/hardhat-zksync-solc";
+import "@matterlabs/hardhat-zksync-deploy"
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
+  //comentar desde aqui para probar los contratos en la red de prueba,osea localhost
+  networks: {
+    zkSyncTestnet: {
+      url: process.env.ALCHEMY_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [`0x${process.env.PRIVATE_KEY}`] : [],
+    },
+  },
+  zksolc: {
+    version: "1.3.5",
+    compilerSource: "binary",
+  },
 };
 
 export default config;
